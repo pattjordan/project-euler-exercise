717dadaf4d490a85f1a0715f5f5b5864b9153f41
Patrick <pj.patrickjordan@gmail.com>
2021-03-31 08:46:18 +0200

    palindrome

--- coding.js
+++ coding.js
@@ -14,6 +14,58 @@ function largestPalindromeProduct(n) {
     // Definition of possible digits
     const largestFactor = Math.pow(10, n) - 1;
     const smallestFactor = Math.pow(10, n - 1);
+
+    let a = largestFactor;
+    let b = largestFactor;
+
+    let palindrome = -1;
+    let mul = -1;
+    while (a >= smallestFactor) {
+      // 99 x 99
+      mul = a * b;
+      console.log("A");
+      console.log(a);
+      console.log(mul);
+      if (isPalindrome(mul)) {
+        palindrome = mul;
+        break;
+      } else {
+        // Decrease b: 99 x 98
+        b -= 1;
+        mul = a * b;
+        console.log("B");
+        console.log(b);
+        console.log(mul);
+        if (isPalindrome(mul)) {
+          palindrome = mul;
+          break;
+        } else {
+          // Decrease a: 98 x 98
+          a -= 1;
+          mul = a * b;
+        }
+      }
+
+      console.log(palindrome);
+    }
+  }
+}
+// Check Palindrom
+function isPalindrome(num) {
+  // Check if reversed number equals number; Return True/False
+  return num.toString() === num.toString().split("").reverse().join("");
+}
+
+largestPalindromeProduct(2);
+
+/*
+function largestPalindromeProduct(n) {
+  if (n == 0) {
+    return 0;
+  } else {
+    // Definition of possible digits
+    const largestFactor = Math.pow(10, n) - 1;
+    const smallestFactor = Math.pow(10, n - 1);
     let palindromes = [];
 
     for (let i = largestFactor; i > smallestFactor; i--) {
@@ -34,3 +86,4 @@ function isPalindrome(num) {
 }
 
 largestPalindromeProduct(3);
+*/
